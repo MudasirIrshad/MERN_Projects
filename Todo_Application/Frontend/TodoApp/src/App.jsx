6f795b41a510418a7f232de7f254938e6a2682f4
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-04-04 12:20:59 +0500

    data fetching from Database

--- Todo_Application/Frontend/TodoApp/src/App.jsx
+++ Todo_Application/Frontend/TodoApp/src/App.jsx
@@ -1,12 +1,21 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
- 
+  const [tasks, setTasks] = useState();
+  useEffect(() => {
+    fetch("http://localhost:3000/tasks", {
+      method: "GET",
+    }).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        setTasks(JSON.stringify(data));
+      });
+    });
+  }, []);
   return (
     <>
-    
+      <p>{tasks}</p>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
