5bb4b4b891486a626c95d99dde4e587dc518ce13
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-04-05 00:33:51 +0500

    Delete button and retrieval technique change

--- Todo_Application/Frontend/TodoApp/src/App.jsx
+++ Todo_Application/Frontend/TodoApp/src/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState();
+  const [tasks, setTasks] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/tasks", {
       method: "GET",
     }).then((response) => {
       response.json().then((data) => {
-        console.log(data);
-        setTasks(JSON.stringify(data));
+        setTasks(data);
       });
     });
   }, []);
   return (
     <>
-      <p>{tasks}</p>
+      <p>{tasks.map((i)=>{
+        return <><li key={i._id}>{i.title+": "+i.description}</li><button style={{background:"white", color:"black"}}>delete</button><br /> </>
+
+      })}</p>
     </>
   );
 }
