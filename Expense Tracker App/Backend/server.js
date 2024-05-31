40568887a55e24bdde8a55099a7d466717f99b4b
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-05-31 23:03:30 +0500

    Expense Route completed

--- Expense Tracker App/Backend/server.js
+++ Expense Tracker App/Backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
-const route = require("./routes/route")
+const express = require("express");
+const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const route = require("./routes/route");
 
-app.use("/",route)
-app.listen(3000)
+app.use("/", route);
+app.listen(3000);
--- Expense Tracker App/Frontend/vite-project/src/Components/Expense.jsx
+++ Expense Tracker App/Frontend/vite-project/src/Components/Expense.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Button, Card } from "@mui/material";
 import TextField from "@mui/material/TextField";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { expenseAmountState, expenseNameState } from "../Atoms";
+import axios from "axios";
 
 export default function Expense() {
+  let name = useRecoilValue(expenseNameState);
+  let amount = useRecoilValue(expenseAmountState);
+  const setName = useSetRecoilState(expenseNameState);
+  const setAmount = useSetRecoilState(expenseAmountState);
   return (
     <div>
       <Card style={{ textAlign: "center", margin: "10px", padding: "10px" }}>
@@ -11,6 +18,9 @@ export default function Expense() {
           label="Name"
           variant="outlined"
           size="small"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
         />
         <br />
         <br />
@@ -19,12 +29,30 @@ export default function Expense() {
           label="Amount"
           variant="outlined"
           size="small"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
         />
         <br />
         <br />
-        <Button variant="contained"> Add Expense </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            axios
+              .post("http://localhost:3000/Expense", {
+                name: name,
+                amount: amount,
+              })
+              .then((res) => {
+                alert(res.data);
+              });
+          }}
+        >
+          Add Expense
+        </Button>
       </Card>
     </div>
   );
 }
 
+function backendRequest_ExpenseDetails() {}
