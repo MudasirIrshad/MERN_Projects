18a961b98e5db2cef003481e69c778ffac662c6a
Mudasir Irshad <mudasirirshad47@gmail.com>
2024-06-02 12:23:49 +0500

    Completed Expense Tracker app with full backend and front end

--- Expense Tracker App/Frontend/vite-project/src/Components/Addition.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Addition() {
-  return (
-    <div>
-      <h1>Addition</h1>
-    </div>
-  )
-}
--- Expense Tracker App/Frontend/vite-project/src/Components/Calculations.jsx
+++ Expense Tracker App/Frontend/vite-project/src/Components/Calculations.jsx
@@ -8,7 +8,10 @@ export default function Calculations() {
   const [Expenses, setExpenses] = useRecoilState(allExpensesState);
   const [Income, setIncome] = useRecoilState(allIncomeState);
   const totalIncomeAmount = Income.reduce((acc, item) => acc + item.amount, 0);
-  const totalExpenseAmount = Expenses.reduce((acc, item) => acc + item.amount, 0);
+  const totalExpenseAmount = Expenses.reduce(
+    (acc, item) => acc + item.amount,
+    0
+  );
 
   const deleteIncome = async (id) => {
     try {
@@ -31,95 +34,98 @@ export default function Calculations() {
     }
   };
   return (
-    <Card
-      style={{
-        display: "flex",
-        justifyContent: "space-around",
-        padding: "10px",
-      }}
-    >
-      <Card style={{ paddingRight: "20px", paddingLeft: "20px" }}>
-        <h3>Expenses</h3>
-        <div style={{ height: "150px", overflow: "auto" }}>
-          {Expenses.map((x) => (
-            <div
-              style={{
-                display: "flex",
-                border: "1px solid black",
-                listStyle: "none",
-                padding: "10px",
-                margin: "5px",
-              }}
-              key={x._id}
-            >
-              <div>
-                <li>{x.name}</li>
-                <li>{x.amount}</li>
-              </div>
-              <Button
+    <div>
+      <Card
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          padding: "10px",
+        }}
+      >
+        <Card style={{ paddingRight: "20px", paddingLeft: "20px" }}>
+          <h3>Expenses</h3>
+          <div style={{ height: "150px", overflow: "auto" }}>
+            {Expenses.map((x) => (
+              <div
                 style={{
-                  marginLeft: "10px",
-                  width: "20px",
-                  height: "30px",
-                  backgroundColor: "red",
+                  display: "flex",
+                  border: "1px solid black",
+                  listStyle: "none",
+                  padding: "10px",
+                  margin: "5px",
                 }}
-                variant="contained"
-                onClick={() => {
-                  deleteExpenses(x._id);
-                }}
+                key={x._id}
               >
-                Delete
-              </Button>
-              <br />
-            </div>
-          ))}
-        </div>
+                <div>
+                  <li>{x.name}</li>
+                  <li>{x.amount}</li>
+                </div>
+                <Button
+                  style={{
+                    marginLeft: "10px",
+                    width: "20px",
+                    height: "30px",
+                    backgroundColor: "red",
+                  }}
+                  variant="contained"
+                  onClick={() => {
+                    deleteExpenses(x._id);
+                  }}
+                >
+                  Delete
+                </Button>
+                <br />
+              </div>
+            ))}
+          </div>
           <h3>Total Expense: {totalExpenseAmount}</h3>
-      </Card>
-      <Card style={{ paddingRight: "20px", paddingLeft: "20px" }}>
-        <h3>Income</h3>
-        <div
-          style={{
-            listStyle: "none",
-            padding: "10px",
-            margin: "5px",
-            height: "130px",
-            overflow: "auto",
-          }}
-        >
-          {Income.map((x) => (
-            <div
-              style={{
-                display: "flex",
-                border: "1px solid black",
-                listStyle: "none",
-                padding: "8px",
-                margin: "5px",
-              }}
-              key={x._id}
-            >
-              <div>
-                <li>{x.name}</li>
-                <li>{x.amount}</li>
-              </div>
-              <Button
+        </Card>
+        <Card style={{ paddingRight: "20px", paddingLeft: "20px" }}>
+          <h3>Income</h3>
+          <div
+            style={{
+              listStyle: "none",
+              padding: "10px",
+              margin: "5px",
+              height: "130px",
+              overflow: "auto",
+            }}
+          >
+            {Income.map((x) => (
+              <div
                 style={{
-                  marginLeft: "10px",
-                  width: "20px",
-                  height: "30px",
-                  backgroundColor: "red",
+                  display: "flex",
+                  border: "1px solid black",
+                  listStyle: "none",
+                  padding: "8px",
+                  margin: "5px",
                 }}
-                variant="contained"
-                onClick={() => deleteIncome(x._id)}
+                key={x._id}
               >
-                Delete
-              </Button>
-              <br />
-            </div>
-          ))}
-        </div>
-        <h3>Total Income: {totalIncomeAmount}</h3>
+                <div>
+                  <li>{x.name}</li>
+                  <li>{x.amount}</li>
+                </div>
+                <Button
+                  style={{
+                    marginLeft: "10px",
+                    width: "20px",
+                    height: "30px",
+                    backgroundColor: "red",
+                  }}
+                  variant="contained"
+                  onClick={() => deleteIncome(x._id)}
+                >
+                  Delete
+                </Button>
+                <br />
+              </div>
+            ))}
+          </div>
+          <h3>Total Income: {totalIncomeAmount}</h3>
+        </Card>
       </Card>
-    </Card>
+        <h3>Remaining Balance: {totalIncomeAmount-totalExpenseAmount}</h3>
+    </div>
   );
 }
